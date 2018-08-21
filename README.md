f801a01aab8ed1f642e737fecdfc64bc253466e9
Carlos <echoulen@gmail.com>
2018-08-21 14:37:38 +0800

    post: add url params

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # test-coverage-plugin
 A test coverage reported plugin with Drone CI
+
+```
+pipeline:
+  test:
+    image: node:9.3-alpine
+    commands:
+      - yarn test 2>&1 | tee output
+      - yarn build
+  report:
+    image: echoulen/test-coverage-plugin:latest
+    file: output
+    repo: My/TestRepo
+    host: 127.0.0.1
+    port: 3200
+    network_mode: host
+```
--- main.go
+++ main.go
@@ -7,9 +7,37 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"github.com/urfave/cli"
+)
+
+var (
+	version = "0.0.0"
+	build   = "0"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "test coverage plugin"
+	app.Usage = "test coverage plugin"
+	app.Action = run
+	app.Version = fmt.Sprintf("%s+%s", version, build)
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "build.link",
+			Usage:  "build.link",
+			EnvVar: "DRONE_BUILD_LINK",
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
+
+}
+
+func run(c *cli.Context) error {
 	file := os.Getenv("PLUGIN_FILE")
 	repo := os.Getenv("PLUGIN_REPO")
 	host := os.Getenv("PLUGIN_HOST")
@@ -20,6 +48,7 @@ func main() {
 	}
 
 	output := fmt.Sprintf("%s", outputReader)
+	buildLink := c.String("build.link")
 
 	fmt.Printf("========= Reporting =========\n")
 	targetUrl := fmt.Sprintf("http://%s:%s/repos/%s", host, port, repo)
@@ -27,12 +56,13 @@ func main() {
 	client := &http.Client{}
 	postValues := url.Values{}
 	postValues.Add("doc", output)
+	postValues.Add("url", buildLink)
 	resp, err := client.PostForm(targetUrl, postValues)
 	fmt.Printf("======= Report finished =======")
 
 	if err != nil {
 		log.Fatal(err)
-		return
+		return nil
 	}
 
 	if resp.StatusCode == 200 {
@@ -41,4 +71,5 @@ func main() {
 	}
 
 	defer resp.Body.Close()
+	return nil
 }
